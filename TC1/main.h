e47bbacdbf1c76e23a78002d0c2c3928ef21c9ae
user <user@example.com>
2026-09-01 01:45:11 +0800

    chore: sync VERSION with .version (v4.1.3)

--- TC1/main.h
+++ TC1/main.h
@@ -21,7 +21,7 @@
 #endif
 #define STR_(x) #x
 #define STR(x) STR_(x)
-#define VERSION "v4.1.0-" STR(COMMIT_HASH)
+#define VERSION "v4.1.3-" STR(COMMIT_HASH)
 
 #define TYPE 1
 #define TYPE_NAME "TC1"
